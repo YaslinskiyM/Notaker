ff16f460b1081b3f35d5722295b4d4d7c8cc2780
maria <mariayaslinskiy@gmail.com>
2023-07-10 19:37:47 -0700

    added routes and server.js

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const api = require('./routes/apiRoutes');
+const html = require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use('/api', api);
+app.use('/', html);
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+    );
